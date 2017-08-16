807d544ffee8cbf24b33f04856c4ec86d6a77743
Yogesh Suhagiya <yogesh.suhagiya@aureatelabs.com>
2017-08-16 12:52:23 +0530

    Complete loading cart items on notification click

---
+++ app/code/community/Frontuser/Integration/Model/Observer.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Frontuser Integration
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * =================================================================
+ *                 MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This software is designed to work with Magento community edition and
+ * its use on an edition other than specified is prohibited. Frontuser does not
+ * provide extension support in case of incorrect edition use.
+ * =================================================================
+ *
+ * =================================================================
+ *                  DISCLAIMER
+ * =================================================================
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Frontuser
+ * @package     Frontuser_Integration
+ * @author Frontuser Team <support@frontuser.com>
+ * @copyright  Copyright (c) 2017 Frontuser. (https://frontuser.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Frontuser_Integration_Model_Observer
+{
+
+	/**
+	 * @param SchemaSetupInterface $setup
+	 * @param ModuleContextInterface $context
+	 */
+	public function upgrade(Varien_Event_Observer $observer)
+	{
+		$quote = $observer->getEvent()->getQuote();
+
+		$token = Mage::helper('core')->getRandomString(32);
+
+		$quote->setFutoken($token);
+
+		return $observer;
+	}
+}
---
+++ app/code/community/Frontuser/Integration/controllers/IndexController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Frontuser Integration
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * =================================================================
+ *                 MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This software is designed to work with Magento community edition and
+ * its use on an edition other than specified is prohibited. Frontuser does not
+ * provide extension support in case of incorrect edition use.
+ * =================================================================
+ *
+ * =================================================================
+ *                  DISCLAIMER
+ * =================================================================
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Frontuser
+ * @package     Frontuser_Integration
+ * @author Frontuser Team <support@frontuser.com>
+ * @copyright  Copyright (c) 2017 Frontuser. (https://frontuser.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Frontuser_Integration_IndexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+	    $code = $this->getRequest()->getParam('code');
+
+	    $quote = Mage::getSingleton('sales/quote')->load($code,'futoken');
+
+	    if(!empty( $quote )) {
+		    $quote->setIsActive(true)->save();
+		    Mage::getSingleton('customer/session')->setQuoteId($quote->getId());
+	    }
+
+	    $this->_redirect('checkout/cart');
+    }
+}
